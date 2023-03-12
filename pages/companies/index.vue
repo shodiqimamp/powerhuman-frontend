a3339048e82049cb7949b84160eccf6bfe03ba00
shodiqimamp <shodiqimamp@gmail.com>
2023-03-12 15:16:14 +0700

    create pages and layouts

---
+++ pages/companies/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="py-[200px] flex flex-col items-center justify-center px-4">
+        <div class="text-[32px] font-semibold text-dark mb-4">Select Companies</div>
+        <div class="w-full card">
+            <div class="form-group">
+                <label for="" class="text-grey">Companies</label>
+                <select  name="companies" id=""
+                    class="appearance-none input-field form-icon-chevron_down">
+                    <option value="" selected>
+                        Company Name
+                    </option>
+                </select>
+            </div>
+            <a href="" class="w-full btn btn-primary mt-[14px]">
+                Continue
+            </a>
+            <div class="text-center">or</div>
+            <NuxtLink :to="{ name: 'companies-create' }" class="w-full border btn btn-white">
+                Create New Company
+            </NuxtLink>
+        </div>
+    </section>
+</template>
